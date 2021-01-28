fd30595fde52139067560202254626b8c33f9097
Kamil Musiał <kmusial40@gmail.com>
2021-01-28 21:10:07 +0100

    rocks

---
+++ Assets/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    public float rotateSpeed;
+    void Update () {
+        rotate();
+    }
+ 
+ 
+    void rotate() {
+        transform.Rotate(Vector3.one, rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
