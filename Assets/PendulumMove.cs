34d0a8faec77600a1d226014f0c3e9b7423af67f
Kamil Musiał <kmusial40@gmail.com>
2021-01-28 02:06:09 +0100

    script for peludium

--- Assets/PendulumMove.cs
+++ Assets/PendulumMove.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PendulumMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _timer = 0f;
+    private float speed = 1f;
+    private int phase = 0;
+    
+    private void FixedUpdate()
     {
-        
-    }
+        _timer += Time.fixedDeltaTime;
+        if (_timer > 1f)
+        {
+            phase++;
+            phase %= 4;
+            _timer = 0f;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        switch (phase)
+        {
+            case 0:
+                transform.Rotate(0f,0f, speed * (1 - _timer));
+                break;
+            case 1:
+                transform.Rotate(0f, 0f, -speed * _timer);
+                break;
+            case 2:
+                transform.Rotate(0f, 0f, -speed * (1 - _timer));
+                break;
+            case 3:
+                transform.Rotate(0f, 0f, speed * _timer);
+                break;
+        }
     }
 }
